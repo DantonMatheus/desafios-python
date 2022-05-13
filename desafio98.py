e09de72ebfb099f58eac39fa045ede26f6c58d22
DantonMatheus <dantonmatheus@gmail.com>
2022-05-13 10:26:32 -0300

    13/05
    Novos exercícios do curso de Python

--- desafio98.py
+++ desafio98.py
@@ -1,1 +1,36 @@
 print('===== DESAFIO 98 =====')
+from time import sleep
+
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print('--' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(1)
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('FIM!')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('--' * 20)
+print('Agora é sua vez de personalizar a contagem!')
+ini = int(input('INÍCIO: '))
+fim = int(input('FIM: '))
+pas = int(input('PASSO: '))
+contador(ini, fim, pas)
